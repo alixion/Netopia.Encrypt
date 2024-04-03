4607184e723f33d046e0eb3ad00ffe267c067952
Alex Manea <ionmanea@gmail.com>
2024-04-03 21:02:44 +0300

    Bug fix in CardPaymentRequestClass
    Invoice property was overwriting the property in the base class

--- src/Netopia.Encrypt/Models/CardPaymentRequest.cs
+++ src/Netopia.Encrypt/Models/CardPaymentRequest.cs
@@ -2,8 +2,6 @@
 
 public class CardPaymentRequest: PaymentRequestBase
 {
-    [XmlElement("invoice")] public PaymentInvoice Invoice { get; set; }
-
     [XmlArrayItem("destination")]
     [XmlArray("split")]
     public List<PaymentSplitDestination> Split { get; set; }
